25152056d17e0a9c9952ab542af23017701917f0
Jonathan Chan <jonathanhwchan@gmail.com>
2020-04-25 14:55:36 -0700

    Added System Fω.

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 This is an implementation of a Church-style (explicitly typed-annotated) System F (polymorphic) lambda calculus with definitions (let-expression) in Redex. Included are:
 
 ### System F
-
 * Type synthesis
 * Head normal form reduction and normal form reduction (both call-by-value)
 * Church encodings of numerals and arithmetic (in progress)
@@ -14,11 +13,17 @@ This is an implementation of a Church-style (explicitly typed-annotated) System 
 * Continuation-plugging
 * Compiler from System F to ANF-restricted System F
 
+### System Fω (extends System F)
+* Why? I don't know.
+* Type synthesis
+* Head normal form, normal form, and type-level reduction
+
 ## TODOs
 * Add tests for ANF translation (type-preservation and correctness)
-* "Uncurry" functions and definitions into multi-parameter/multi-binding syntactic forms
+* Add tests for System Fω (type synthesis, reduction)
+* "Uncurry" functions and definitions into multi-parameter/multi-binding syntactic forms (F-ANF)
 * Implement System F with closures and abstract closure conversion from F-ANF to F-ACC
 * Fix ambiguity of ANF evaluation context
-* Add evaluation context for call-by-name evaluation
-* Finish Church encodings
+* Add evaluation context for call-by-name evaluation (F, F-ANF, Fω)
+* Finish Church encodings (F)
 * Add inventory of metafunctions to this README
---
+++ system-f-omega.rkt
@@ -0,0 +1,203 @@
+#lang racket
+
+(require (rename-in (except-in "./system-f.rkt" λ* ∀* Δ* ∈Δ)
+                    [⊢ F.⊢]
+                    [⟶ F.⟶])
+         (rename-in redex/reduction-semantics
+                    ;; This is obviously the correct spelling of "judgement"
+                    [define-judgment-form          define-judgement-form]
+                    [define-extended-judgment-form define-extended-judgement-form]
+                    [judgment-holds                judgement-holds]))
+
+(module+ test
+  (require (rename-in redex-chk
+                      [redex-judgment-holds-chk redex-judgement-holds-chk])))
+
+(provide (all-defined-out))
+
+;; SYSTEM Fω ;;
+
+;; Syntax
+
+(define-extended-language λFω λF
+  (κ ι ::= * (⇒ κ κ))
+  (τ σ ::= .... (Λ (α : κ) τ) (∀ (α : κ) τ) (τ τ)) ;; Types
+  (e   ::= .... (Λ (α : κ) e)) ;; Terms
+  (v   ::= .... (Λ (α : κ) e)) ;; Term values
+  (w   ::= α (Λ (α : κ) τ)) ;; Type values
+  (Δ   ::= · (Δ (α : κ))) ;; Type contexts
+  (F   ::= .... (Λ (α : κ) F)) ;; Evaluation contexts (normal form)
+  (G   ::= hole (G τ) (w G)) ;; Evaluation contexts (types)
+
+  #:binding-forms
+  (∀ (α : κ) τ #:refers-to α)
+  (Λ (α : κ) e #:refers-to α))
+
+(default-language λFω)
+
+;; Unroll (λ* ([a_1 : b_1] ... [a_n : b_n]) e) into (L [a_1 : b_1] ... (L [a_n : b_n] e))
+;; where (L ::= λ Λ) (a ::= [x : τ] [α : κ])
+(define-metafunction λFω
+  λ* : ([any : any] ...) e -> e
+  [(λ* () e) e]
+  [(λ* ([x : τ] any ...) e)
+   (λ (x : τ) (λ* (any ...) e))]
+  [(λ* ([α : κ] any ...) e)
+   (Λ (α : κ) (λ* (any ...) e))])
+
+;; Unroll (Λ* ([α_1 : κ_1] ... [α_n : κ_n]) τ) into (Λ (α_1 : κ_1) ... (Λ (α_n : κ_n) τ))
+(define-metafunction λFω
+  Λ* : ([α : κ] ...) τ -> τ
+  [(Λ* () τ) τ]
+  [(Λ* ([α : κ] [α_r : κ_r] ...) τ)
+   (Λ (α : κ) (Λ* ([α_r : κ_r] ...) τ))])
+
+;; Unroll (@* τ τ_1 ... τ_n) into ((e τ_1) ... τ_n)
+(define-metafunction λFω
+  @* : any ... -> e
+  [(@* τ) τ]
+  [(@* τ_1 τ_2 any ...)
+   (@* (τ_1 τ_2) any ...)])
+
+;; Unroll (κ_1 ⇒ ... ⇒ κ_n) into (κ_1 ⇒ (... ⇒ κ_n))
+(define-metafunction λFω
+  ⇒* : κ ... κ -> κ
+  [(⇒* κ) κ]
+  [(⇒* κ κ_r ...)
+   (⇒ κ (⇒* κ_r ...))])
+
+;; Unroll (∀* ([α_1 : κ] ... [α_n : κ]) τ) as (∀ (α_1 : κ_1) ... (∀ (α_n : κ_n) τ))
+(define-metafunction λFω
+  ∀* : ([α : κ] ...) τ -> τ
+  [(∀* () τ) τ]
+  [(∀* ([α : κ] [α_r : κ_r] ...) τ)
+   (∀ (α : κ) (∀* ([α_r : κ_r] ...) τ))])
+
+;; Unroll ([α_1 : κ] ... [α_n : κ]) into ((· (α_1 : κ_1)) ... (α_n : κ_n))
+(define-metafunction λFω
+  Δ* : [α : κ] ... -> Δ
+  [(Δ*) ·]
+  [(Δ* (α_r : κ_r) ... (α : κ))
+   ((Δ* (α_r : κ_r) ...) (α : κ))])
+
+(module+ test
+  (redex-chk
+   (λ* ([x : a] [b : *] [z : c] [d : (⇒ * *)]) x) (λ (x : a) (Λ (b : *) (λ (z : c) (Λ (d : (⇒ * *)) x))))
+   (Λ* ([a : *] [b : (⇒ * *)]) (b a)) (Λ (a : *) (Λ (b : (⇒ * *)) (b a)))
+   (@* a b c d) (((a b) c) d)
+   (⇒* (⇒ * *) (⇒* * * *) *) (⇒ (⇒ * *) (⇒ (⇒ * (⇒ * *)) *))
+   (∀* ([a : *] [b : (⇒ * *)]) (b a)) (∀ (a : *) (∀ (b : (⇒ * *)) (b a)))
+   (Δ* [a : *] [b : (⇒ * *)]) ((· (a : *)) (b : (⇒ * *)))))
+
+
+;; Static Semantics
+
+;; (α : κ) ∈ Δ
+(define-judgement-form λFω
+  #:contract (∈Δ α κ Δ)
+  #:mode (∈Δ I O I)
+
+  [------------- "Δ-car"
+   (∈Δ α κ (Δ (α : κ)))]
+
+  [(∈Δ α κ Δ)
+   --------------- "Δ-cdr"
+   (∈Δ α κ (Δ (α_0 : κ_0)))])
+
+(module+ test
+  (redex-judgement-holds-chk
+   ∈Δ
+   [#:f a * ·]
+   [#:f a * (· (b : *))]
+   [#:f a (⇒ * *) (· (a : *))]
+   [a * (· (a : *))]
+   [a (⇒ * *) (· (a : (⇒ * *)))]
+   [a * (Δ* (a : *) (b : (⇒ * *)))]
+   [a * (Δ* (a : (⇒ * *)) (a : *))]))
+
+;; Δ ⊢ τ : κ
+(define-judgement-form λFω
+  #:contract (⊢τ Δ τ κ)
+  #:mode (⊢τ I I O)
+
+  [(∈Δ α κ Δ)
+   ----------- "τ-var"
+   (⊢τ Δ α κ)]
+
+  [(⊢τ (Δ (α : ι)) τ κ)
+   ------------------------------ "τ-fun"
+   (⊢τ Δ (Λ (α : ι) τ) (⇒ ι κ))]
+
+  [(⊢τ Δ σ ι)
+   (⊢τ Δ τ (⇒ ι κ))
+   --------------- "τ-app"
+   (⊢τ Δ (τ σ) κ)]
+
+  [(⊢τ Δ σ *)
+   (⊢τ Δ τ *)
+   ----------------- "τ-arr"
+   (⊢τ Δ (→ σ τ) *)]
+
+  [(⊢τ (Δ (α : κ)) τ *)
+   ------------------------ "τ-poly"
+   (⊢τ Δ (∀ (α : κ) τ) *)])
+
+;; Δ Γ ⊢ e : τ
+(define-extended-judgement-form λFω F.⊢
+  #:contract (⊢ Δ Γ e τ)
+  #:mode (⊢ I I I O)
+  
+  [(⊢τ Δ σ *)
+   (⊢ Δ (Γ (x : σ)) e τ)
+   ------------------------------------ "fun"
+   (⊢ Δ Γ (λ (x : σ) e) (→ σ τ))]
+
+  [(⊢ (Δ (α : κ)) Γ e τ)
+   ------------------------- "polyfun"
+   (⊢ Δ Γ (Λ (α : κ) e) (∀ (α : κ) τ))]
+
+  [(⊢τ Δ σ κ)
+   (⊢ Δ Γ e (∀ (α : κ) τ))
+   ------------------------------------- "polyapp"
+   (⊢ Δ Γ (e [σ]) (substitute τ α σ))])
+
+
+;; Dynamic Semantics
+
+(define ⟶
+  (extend-reduction-relation
+   F.⟶ λFω
+   (--> ((Λ (α : κ) e) [τ])
+        (substitute e α τ)
+        "τ")))
+
+(define ⟶*
+  (context-closure ⟶ λFω E))
+
+(define-metafunction λFω
+  reduce : e -> v
+  [(reduce e)
+   ,(first (apply-reduction-relation* ⟶* (term e) #:cache-all? #t))])
+
+(define ⇓
+  (context-closure ⟶ λFω F))
+
+(define-metafunction λFω
+  normalize : e -> v
+  [(normalize e)
+   ,(first (apply-reduction-relation* ⇓ (term e) #:cache-all? #t))])
+
+(define ⟹
+  (reduction-relation
+   λFω
+   (--> ((Λ (α : κ) τ) w)
+        (substitute τ α w)
+        "β")))
+
+(define ⟹*
+  (context-closure ⟹ λFω G))
+
+(define-metafunction λFω
+  reduce-type : e -> v
+  [(reduce-type e)
+   ,(first (apply-reduction-relation* ⟹* (term e) #:cache-all? #t))])
